ff6a06e6b1dc710548905858510405e621cb9e66
Karthik R <karthikr1493@gmail.com>
2020-03-23 19:13:27 +0530

    Enable Data Binding Show toast message upon click of refresh button.

--- app/src/main/java/com/android_proficiency_exercise/MainActivity.kt
+++ app/src/main/java/com/android_proficiency_exercise/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.android_proficiency_exercise
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import android.widget.Toast.LENGTH_LONG
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.android_proficiency_exercise.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.refreshButton.setOnClickListener {
+            Toast.makeText(applicationContext, "Button is clicked", LENGTH_LONG).show()
+        }
     }
 }
